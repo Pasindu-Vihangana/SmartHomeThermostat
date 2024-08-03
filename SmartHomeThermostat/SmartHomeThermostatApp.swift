cec72ced4862d9fd8a1e8c9cfa90463959ba8034
Pasindu Vihangana <pasi1028@gmail.com>
2024-08-03 21:43:43 +0530

    Initial Commit

---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Pasindu Vihangana on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
